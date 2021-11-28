2c684772fb38249eea5b0eb817f39963d37e7ee7
SImon Hinterseer <hinterseer.simon@gmail.com>
2021-11-28 11:43:00 +0100

    ex06 - the two inclusive scans are done

--- ex06/ex06_1.cpp
+++ ex06/ex06_1.cpp
@@ -4,8 +4,8 @@
 #include <iostream>
 #include <stdio.h>
 
-#define GRID_SIZE 256
-#define BLOCK_SIZE 256
+#define GRID_SIZE 4
+#define BLOCK_SIZE 8
 
 
 
@@ -14,7 +14,7 @@ __global__ void scan_kernel_1(double const *X,
                               int N,
                               double *carries)
 {
-  __shared__ double shared_buffer[256];
+  __shared__ double shared_buffer[BLOCK_SIZE];
   double my_value;
 
   unsigned int work_per_thread = (N - 1) / (gridDim.x * blockDim.x) + 1;
@@ -57,10 +57,61 @@ __global__ void scan_kernel_1(double const *X,
 
 }
 
+__global__ void scan_kernel_1_incl(double const *X,
+                              double *Y,
+                              int N,
+                              double *carries)
+{
+  __shared__ double shared_buffer[BLOCK_SIZE];
+  double my_value;
+
+  unsigned int work_per_thread = (N - 1) / (gridDim.x * blockDim.x) + 1;
+  unsigned int block_start = work_per_thread * blockDim.x *  blockIdx.x;
+  unsigned int block_stop  = work_per_thread * blockDim.x * (blockIdx.x + 1);
+  unsigned int block_offset = 0;
+
+  // run scan on each section
+  for (unsigned int i = block_start + threadIdx.x; i < block_stop; i += blockDim.x)
+  {
+    // load data:
+    my_value = (i < N) ? X[i] : 0;
+
+    // inclusive scan in shared buffer:
+    for(unsigned int stride = 1; stride < blockDim.x; stride *= 2)
+    {
+      __syncthreads();
+      shared_buffer[threadIdx.x] = my_value;
+      __syncthreads();
+      if (threadIdx.x >= stride)
+        my_value += shared_buffer[threadIdx.x - stride];
+    }
+    __syncthreads();
+    shared_buffer[threadIdx.x] = my_value;
+    __syncthreads();
+
+    // exclusive scan requires us to write a zero value at the beginning of each block
+    //my_value = (threadIdx.x > 0) ? shared_buffer[threadIdx.x - 1] : 0;
+
+    // write to output array
+    if (i < N)
+      Y[i] = block_offset + my_value;
+
+    block_offset += shared_buffer[blockDim.x-1];
+  }
+
+  // write carry:
+  if (threadIdx.x == 0)
+    carries[blockIdx.x] = block_offset;
+
+}
+
+
+
+
 // exclusive-scan of carries
 __global__ void scan_kernel_2(double *carries)
 {
-  __shared__ double shared_buffer[256];
+  __shared__ double shared_buffer[GRID_SIZE];
 
   // load data:
   double my_carry = carries[threadIdx.x];
@@ -114,6 +165,7 @@ void exclusive_scan(double const * input,
   cudaMalloc(&carries, sizeof(double) * GRID_SIZE);
 
   // First step: Scan within each thread group and write carries
+  //scan_kernel_1<<<GRID_SIZE, BLOCK_SIZE>>>(input, output, N, carries);
   scan_kernel_1<<<GRID_SIZE, BLOCK_SIZE>>>(input, output, N, carries);
 
   // Second step: Compute offset for each thread group (exclusive scan for each thread group)
@@ -126,6 +178,29 @@ void exclusive_scan(double const * input,
 }
 
 
+void inclusive_scan2(double const * input,
+                    double       * output, int N)
+{
+
+  double *carries;
+  cudaMalloc(&carries, sizeof(double) * GRID_SIZE);
+
+  // First step: Scan within each thread group and write carries
+  //scan_kernel_1<<<GRID_SIZE, BLOCK_SIZE>>>(input, output, N, carries);
+  scan_kernel_1_incl<<<GRID_SIZE, BLOCK_SIZE>>>(input, output, N, carries);
+
+  // Second step: Compute offset for each thread group (exclusive scan for each thread group)
+  scan_kernel_2<<<1, GRID_SIZE>>>(carries);
+
+  // Third step: Offset each thread group accordingly
+  scan_kernel_3<<<GRID_SIZE, BLOCK_SIZE>>>(output, N, carries);
+
+  cudaFree(carries);
+}
+
+
+
+
 __global__
 void device_excl_to_incl(const double *device_input, double *device_output, int N)
 {
@@ -136,8 +211,7 @@ void device_excl_to_incl(const double *device_input, double *device_output, int 
 		device_output[i] += device_input[i];	
 }
 
-
-void inclusive_scan(const double *device_input, double *device_output, int N)
+void inclusive_scan1(const double *device_input, double *device_output, int N)
 {
 	// get exclusive scan
   exclusive_scan(device_input, device_output, N);
@@ -174,8 +248,10 @@ int main() {
 
   // Perform the exclusive scan and obtain results
   //exclusive_scan(cuda_x, cuda_y, N);
+  //inclusive_scan1(cuda_x, cuda_y, N);
+  inclusive_scan2(cuda_x, cuda_y, N);
 	//cudaDeviceSynchronize();
-	inclusive_scan(cuda_x, cuda_y, N);
+	//inclusive_scan(cuda_x, cuda_y, N);
   cudaMemcpy(z, cuda_y, sizeof(double) * N, cudaMemcpyDeviceToHost);
 
   //
