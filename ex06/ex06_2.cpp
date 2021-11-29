481b5063051dde8d368ca7939fba357c36bcedf8
SImon Hinterseer <hinterseer.simon@gmail.com>
2021-11-29 13:47:38 +0100

    Ex06 - p2 - matrix assembly works

--- ex06/ex06_2.cpp
+++ ex06/ex06_2.cpp
@@ -64,8 +64,6 @@ void assembleA(int *csr_rowoffsets, double *csr_values, int* csr_colindices, int
 }
 
 
-
-
 //__global__
 void num_nonzero_entries(int N, int M, int *output){
 //void num_nonzero_entries(int N, int M, double *output){
@@ -147,8 +145,6 @@ __global__ void scan_kernel_1(int const *X,
     carries[blockIdx.x] = block_offset;
 
 }
-
-
 
 
 
@@ -287,34 +283,28 @@ double execution_wrapper(int grid_size,
 }
 
 
-
 // _________________________________ Main ________________________________
 // ________________________________________________________________________
 
 int main() {
 	
 	int N = 3;
-	int M = 4;
+	int M = 3;
 	bool sanity_check = true;
 	
 	int *num_entries = (int *)malloc(sizeof(int) * N*M);
 	int *csr_rowoffsets = (int *)malloc(sizeof(int) * N*M);
-	//double *num_entries = (double *)malloc(sizeof(double) * N*M);
-	//double *csr_rowoffsets = (double *)malloc(sizeof(double) * N*M + 1);
 	
 	int* device_num_entries;
 	cudaMalloc(&device_num_entries, sizeof(int) * N*M);
-	//double* device_num_entries;
-	//cudaMalloc(&device_num_entries, sizeof(double) * N*M);
 	
 	int* device_csr_rowoffsets;
 	cudaMalloc(&device_csr_rowoffsets, sizeof(int) * N*M);
-	//double* device_csr_rowoffsets;
-	//cudaMalloc(&device_csr_rowoffsets, sizeof(double) * N*M + 1);
 	
 
 	// get number of nonzero entries per row
 	num_nonzero_entries(N, M, num_entries);
+
 	//if (sanity_check){
 	if (false){
 		for(int i = 0; i < N*M; ++i){
@@ -323,21 +313,24 @@ int main() {
 		std::cout << std::endl;
 	}
 
-	cudaMemcpy(device_num_entries, num_entries, sizeof(int) * N*M, cudaMemcpyHostToDevice);
-	//cudaMemcpy(device_num_entries, num_entries, sizeof(double) * N*M, cudaMemcpyHostToDevice);
-	
+	cudaMemcpy(device_num_entries, num_entries, sizeof(int) * N*M, cudaMemcpyHostToDevice);	
 	
 	// get row offsets for CSR as exclusive prefix sum of num_entries	
 	exclusive_scan(device_num_entries, device_csr_rowoffsets, N*M);
 		
 	cudaMemcpy(csr_rowoffsets, device_csr_rowoffsets, sizeof(int) * N*M, cudaMemcpyDeviceToHost);
-	//cudaMemcpy(csr_rowoffsets, device_csr_rowoffsets, sizeof(double) * N*M, cudaMemcpyDeviceToHost);
 	csr_rowoffsets[N*M] = csr_rowoffsets[N*M - 1] + num_entries[N*M - 1];
+	
+	
+	int *csr_colindices = (int *)malloc(sizeof(int) * csr_rowoffsets[N*M]);
+	double *csr_values = (double *)malloc(sizeof(double) * csr_rowoffsets[N*M]);
 
+	assembleA(csr_rowoffsets, csr_values, csr_colindices, N, M);
+	//get_system_matrix(csr_rowoffsets, csr_values, csr_colindices, N, M);
 
 	if (sanity_check){
 		for(int i = 0; i < N*M; ++i){
-			std::cout << num_entries[i] << ", ";
+			//std::cout << num_entries[i] << ", ";
 		}
 		std::cout << std::endl;
 		for(int i = 0; i < N*M+1; ++i){
@@ -346,15 +339,6 @@ int main() {
 		std::cout << std::endl;
 	}
 
-
-	
-//void assembleA(int *csr_rowoffsets, double *csr_values, int* csr_colindices, int N, int M) {
-	
-	
-	int *csr_colindices = (int *)malloc(sizeof(int) * csr_rowoffsets[N*M]);
-	double *csr_values = (double *)malloc(sizeof(double) * csr_rowoffsets[N*M]);
-
-	assembleA(csr_rowoffsets, csr_values, csr_colindices, N, M);
 
 	if (sanity_check){
 		std::cout << "csr_colindices: " << std::endl;
@@ -367,6 +351,29 @@ int main() {
 			std::cout << csr_values[i] << ", ";
 		}
 		std::cout << std::endl;
+		
+		for(int i = 1; i < 10; ++i){
+			csr_values[i] = 0;
+		}
+		
+		generate_fdm_laplace(N, csr_rowoffsets, csr_colindices, csr_values);
+		
+		
+		std::cout << "correct values:  " << std::endl;
+		
+		std::cout << "csr_colindices: " << std::endl;
+		for(int i = 0; i < csr_rowoffsets[N*M]; ++i){
+			std::cout << csr_colindices[i] << ", ";
+		}
+		std::cout << std::endl;
+		std::cout << "csr_values: " << std::endl;
+		for(int i = 0; i < csr_rowoffsets[N*M]; ++i){
+			std::cout << csr_values[i] << ", ";
+		}
+		std::cout << std::endl;
+		
+			
+		
 	}
 
 
